b9f4bcca0ae8240c475d622aa73967206b502b09
dgm <vsl_sv@bk.ru>
2019-10-15 23:19:13 +0300

    feat: add solution for factorial

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function zeros(expression) {
-  // your solution
+  
+n = parseInt(expression);
+
+
+
+  function number(n) {
+    var counter = 0;
+    for (var i = 5; n/i >= 1; i *= 5)
+      counter += Math.floor(n/i);
+    return counter;
+  }
+
+
+
+  return number(n);
+
+
+  /*
+  let counter = 0;
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+  }*/
+
 }
