f1b45c683e6398ab44319272e95a6029ddcc7db4
dgm <vsl_sv@bk.ru>
2019-10-17 11:30:15 +0300

    refactor: seacrh errors

--- src/index.js
+++ src/index.js
@@ -2,34 +2,85 @@ module.exports = function zeros(expression) {
 
   const arrayFromExpression = expression.split("*"); // получаем массив элементов
   
-  const doubleFactorial = arrayFromExpression.filter(function(elem){ // получаем только двойные факториалы
+  const doubleFactorial = arrayFromExpression.filter(function(elem){
+    // получаем только двойные факториалы
     if (elem.slice(-2) == "!!")
     return elem;
   });
 
-  const simpleFactorial =  arrayFromExpression.filter(function(elem){ // получаем только простые факториалы
+  const simpleFactorial =  arrayFromExpression.filter(function(elem){
+    // получаем только простые факториалы
     if (elem.slice(-2) != "!!")
     return elem;
   });
 
-  const doubleFiltered = doubleFactorial.map(function(element){ // переводим строку в число
+  const doubleFiltered = doubleFactorial.map(function(element){
+    // переводим строку в число
       return parseInt(element, 10);
   });
 
-  const simpleFiltered = simpleFactorial.map(function(element){ // переводим строку в число
+  const simpleFiltered = simpleFactorial.map(function(element){
+    // переводим строку в число
     return parseInt(element, 10);
 });
 
-
   const nullOfDouble = doubleFiltered.map(function(num){
+    // вычисляем количество нулей
     if (num % 2) {return 0;}
     else {
-      // тут надо вычислить
+      function number1(n) {
+          let counter = 0;
+          for (let i = 10; n/i >= 1; i *= 10)
+            counter += Math.floor(n/i);
+          return counter;
+        }
+      return number1(num);
     }
 
   });
 
+//console.log(nullOfDouble);
 
+const nullOfSimple = simpleFiltered.map(function(num){
+  // вычисляем количество нулей
+    return number(num);
+
+      function number(n) {
+        var counter = 0;
+        for (var i = 5; n/i >= 1; i *= 5)
+          counter += Math.floor(n/i);
+        return counter;
+      }  
+});
+
+//console.log(nullOfSimple);
+
+var k = 0;
+var a = 0;
+
+while (k < (nullOfSimple.length - 1)) {
+  a = a + nullOfSimple[k];
+  k++;
+}
+
+var k = 0;
+var b = 0;
+
+while (k < (nullOfDouble.length - 1)) {
+  b = b + nullOfDouble[k];
+  k++;
+}
+
+
+//let a = nullOfSimple[3] != undefined ? nullOfSimple[3] : 0; + nullOfSimple[4] != undefined ? nullOfSimple[4] : 0;
+
+
+
+console.log(a + b);
+return a+b;
+
+
+/*
   let arr = nullOfDouble;
   let sum = 0;
   
@@ -37,10 +88,16 @@ module.exports = function zeros(expression) {
     sum += elem;
   });
   
+  let arr = nullOfSimple;
+    
+  arr.forEach(function(elem) {
+    sum += elem;
+  });
+
   console.log(sum);
   return sum;
 
-  
+  */
 
 
 //let n = parseInt(expression);
@@ -67,3 +124,28 @@ module.exports = function zeros(expression) {
   }*/
 
 }
+/*
+
+получить строку
+
+порезать на куски
+
+отсортировать, сложить двойные факториалы в одну, а обычные в другую переменную
+
+если двойной Ф. нечётный, то вычисление не требуется, если чётный вычислить кол-во нулей
+
+вычислить кол-во нулей для обычного Ф.
+
+сложить кол-во
+
+вернуть результат
+
+
+
+
+
+
+
+
+
+*/
